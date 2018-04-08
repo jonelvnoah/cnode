1555604dac5eb46868d1dff7c0bd6763a5881178
jonelvnoah <jonelvnoah@gmail.com>
2018-04-07 22:24:01 -0400

    2018/04/07
    2018/04/07

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,60 +1,41 @@
-;(function (global) {
+(function(global){
 
-  function createXhr() {
-    var xhr
-    try {
-      xhr = new ActiveXObject("microsoft.xmlhttp")
-    } catch(e1) {
-      try {
-        xhr = new XMLHttpRequest()
-      } catch(e2) {
-         window.alert("您的浏览器不支持ajax")
-      }
+  global.ajax = function(config){
+
+    var type = config.type || 'GET';
+    var url = config.url;
+    var success = config.success;
+    var fail = config.fail;
+    var xhr = new XmlHttpRequest();
+
+    xhr.open(type,url,true)
+
+
+    if (type === 'GET') {
+      xhr.send(null)
+    }else{
+      xhr.send(data)
     }
-    return xhr
-  }
 
-  function jsonToForm(json) {
-    var str = ''
-    for (key in json) {
-      str += (key + '=' + json[key] + '&')
-    }
-    str = str.slice(0, str.length - 1)
-    return str
-  }
-
-  var util = {
-    ajax: function (config) {
-      var url = config.url
-      var data = config.data
-      var success = config.success
-      var fail = config.fail
-      var type = (config.type || 'get').toLowerCase()
-
-      var xhr = createXhr()
-
-      xhr.open(type, url, true)
-
-      if (type === 'get') {
-        xhr.send(null)
-      } else {
-        xhr.setRequestHeader("content-type","application/x-www-form-urlencoded")
-        xhr.send(typeof data === 'string' ? data : jsonToForm(data))
+    xhr.onreadystatechange = function(){
+      if( xhr.status === 200){
+        success(xhr.responseText)
+      }else{
+        fail(xhr.responseText)
       }
+    };
 
-      xhr.onreadystatechange = function () {
-        if(xhr.readyState == 4){
-          if (xhr.status == 200) {
-            success(JSON.parse(xhr.responseText), xhr)
-          } else {
-            fail(xhr.responseText, xhr)
-          }
-        }
-      }
-
-    }
-  }
-
-  global._ = global.util = util
-
+  };
+  
 })(window)
+
+window.ajax({
+  type: 'POST',
+  url: '/api/login',
+  success: function(resp){
+    alert('Success!');
+  },
+  fail: function(resp){
+    alert('Fail!')
+  }
+})
---
+++ public/javascripts/index.js
@@ -0,0 +1,25 @@
+var tabList = document.querySelectorAll('#tabs .tab');
+var topicList = document.querySelectorAll('#topics .topic');
+
+function filterTopics(type){
+  for(var i = 0; i<topicList.length; i++){
+    var topic = topicList[i];
+    if(type === 'all'){
+      topic.style.display = 'block';
+    }else if(topic.getAttribute('tab') === type){
+        topic.style.display = 'block'
+    }else{
+        topic.style.display = 'none'
+      }
+    }
+  }
+
+for(var i=0; i<tabList.length; i++){
+  tabList[i].addEventListener('click', function(event) {
+    /* Act on the event */
+    var tab = event.target.parentNode;
+    var type = tab.getAttribute('tab');
+
+    filterTopics(type);
+  });
+}
