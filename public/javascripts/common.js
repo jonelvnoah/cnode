73a77596d6c11f43b5eb7ad966169294e3183310
jonelvnoah <jonelvnoah@gmail.com>
2018-04-09 15:05:40 -0400

    2018/04/09
    2018/04/09

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,5 +1,6 @@
 (function(global){
 
+  var util = {
   global.ajax = function(config){
 
     var type = config.type || 'GET';
@@ -26,7 +27,7 @@
     };
 
   };
-  
+
 })(window)
 
 window.ajax({
@@ -38,4 +39,39 @@ window.ajax({
   fail: function(resp){
     alert('Fail!')
   }
+
+  var navLoginItem = document.querySeletor('#nav-login-item > a');
+  var loginDropdown = document.getElementById('user-login-dropdown');
+  var loginButton = document.getElementById('login-button');
+  var loginInput = document.getElementById('input-token');
+  var loginUserInfo = document.querySeletor('.login-user-info');
+  var loginUserAvatar = document.querySeletor('.login-user-avatar');
+  var loginUserName = document.querySeletor('.login-user-name');
+  var notLogin = document.querySeletor('.not-login')
+  navLoginItem.addEventListener('click',function(e){
+    e.preventDefault();
+    var display = loginDropdown.style.display
+    if(e.target)
+      display === 'block';
+    else{
+      display === 'none';
+    }
+  })
+
+  loginButton.addEventListener('click',function(e){
+    e.preventDefault();
+    var token = loginInput.value;
+    util.ajax({
+      type: 'POST',
+      url: '/api/login',
+      data: {
+        accesstoken: token;
+      },
+      success: function(user){
+        if(user.success){
+          window.location.reload();
+        }
+      }
+    })
+  })
 })
